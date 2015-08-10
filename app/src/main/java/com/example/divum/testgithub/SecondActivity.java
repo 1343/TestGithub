d225e3582b4f2b8a07c8d248033ea79d886d811e
Gopal kumar <gopal.kumar353@gmail.com>
2015-08-10 11:52:37 +0530

    two  new activity_xml is  added

--- app/src/main/java/com/example/divum/testgithub/SecondActivity.java
+++ app/src/main/java/com/example/divum/testgithub/SecondActivity.java
@@ -1,15 +1,7 @@
 package com.example.divum.testgithub;
 
-import android.app.Activity;
-import android.os.Bundle;
-
 /**
  * Created by divum on 10/8/15.
  */
-public class SecondActivity extends Activity{
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+public class SecondActivity {
 }
