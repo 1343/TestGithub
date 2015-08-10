f465cb37a73199f7d998fa426d27b4a59a05312f
Gopal kumar <gopal.kumar353@gmail.com>
2015-08-10 11:06:02 +0530

    two  new activity added

--- app/src/main/java/com/example/divum/testgithub/SecondActivity.java
+++ app/src/main/java/com/example/divum/testgithub/SecondActivity.java
@@ -1,7 +1,15 @@
 package com.example.divum.testgithub;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by divum on 10/8/15.
  */
-public class SecondActivity {
+public class SecondActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
